fcdf9dcae574c0006e4659347cc6772357936229
TijilM <tijilmalhotra@gmail.com>
2024-06-15 17:39:57 +0530

    react file structure created

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          {/* <Route exact path="/home" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
