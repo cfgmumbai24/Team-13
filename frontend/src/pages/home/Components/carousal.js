a38e0ae9f4749a7e3365a27ef4f347550e778ff2
arundhatishukla1 <ad2088@srmist.edu.in>
2024-06-15 23:19:05 +0530

    added images to carousal

--- frontend/src/pages/home/Components/carousal.js
+++ frontend/src/pages/home/Components/carousal.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Make sure to import Bootstrap 
 import styles from '../Style/CarouselComponent.module.css'; // Import CSS module for additional styling
 import pramod from '../Assets/pramod.jpg';
 import img1 from '../Assets/tulsi.png';
+import img2 from '../Assets/harish.jpg';
 
 const CarouselComponent = () => {
   return (
@@ -15,10 +16,9 @@ const CarouselComponent = () => {
             src={img1}
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3 className={styles.carouselText}>First slide label</h3>
-            <p className={styles.carouselText}>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+            <h3 className={styles.carouselTextH}><br className="small-space" /> Tulsi, from Mukteshwar</h3>
+            <p className={styles.carouselText}>Tulsi balances roles as a teacher, farmer, and trekking instructor, and has also started a homestay for tourists in her village. Despite limited schooling, she ventured into social work with Chirag's Room to Read program and leads treks in Nepal.</p>
+          
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -26,21 +26,21 @@ const CarouselComponent = () => {
             src={pramod}
             alt="Second slide"
           />
-          <Carousel.Caption>
-            <h3 className={styles.carouselText}>Second slide label</h3>
-            <p className={styles.carouselText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          
+            <h3 className={styles.carouselTextH}><br className="small-space" />Pramod Soun, from Badari</h3>
+            <p className={styles.carouselText}>Pramod Soun launched his mushroom cultivation business in March 2022 after attending awareness workshops and rejecting a traditional job. With guidance from Margshala's network, he achieved his first successful sale of oyster mushrooms in May 2022, marking his venture into the local market.</p>
+          
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            // src={image1}
+            src={img2}
             alt="Third slide"
           />
-          <Carousel.Caption>
-            <h3 className={styles.carouselText}>Third slide label</h3>
-            <p className={styles.carouselText}>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
+          
+            <h3 className={styles.carouselTextH}><br className="small-space" />Harish</h3>
+            <p className={styles.carouselText}>Harish diverged from his family's military tradition to embrace entrepreneurship for long-term stability, launching a profitable business through the Margshala program. He values this path as a stable alternative to the uncertain availability of government jobs.</p>
+          
         </Carousel.Item>
       </Carousel>
     </div>
