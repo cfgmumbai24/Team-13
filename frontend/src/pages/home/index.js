7f45baade82e1daee6f936bee14fac026c902cc5
mitsil-27 <grovermitsil27@gmail.com>
2024-06-15 23:26:53 +0530

    added color to navbar

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "./Components/navbar";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CarouselComponent from "./Components/carousal";
