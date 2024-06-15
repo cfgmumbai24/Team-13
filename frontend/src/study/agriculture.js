4c7539e9e7e8416f94c9a9135a499753b882496d
Cdaga8 <chaityadaga2003@gmail.com>
2024-06-16 00:07:05 +0530

    working on studyfolder

---
+++ frontend/src/study/agriculture.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import Navbar from './components/navbar';
+
+
+const DairyFarmingContent = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>LOCAL LIVELIHOODS - DAIRY FARMING</h1>
+      <h2>AGRICULTURE - ANIMAL HUSBANDRY</h2>
+      <p>
+        Animal husbandry is a subset of farm-based livelihoods which includes breeding
+        and care of farm animals such as cattle, sheep, goats, ponies, etc. It is referred to as
+        livestock raising for commercial purposes to meet the high demands of food.
+        Besides being a traditional occupation integral to the functioning and food supply to
+        the whole country, it is the major contributor to the rural economy and small and
+        marginal or landless farmers. Animal husbandry has four major types- dairy,
+        poultry, fish, and beekeeping. It helps the communities by providing dairy products,
+        meat, fibre, manure, it diversifies the land and created employment opportunities.
+      </p>
+      <p>
+        Dairy farming is the keeping of milch animals which produce milk such as cows,
+        buffaloes, and goats. It is also a considerable source for the nutritional security of
+        the agricultural communities. The white revolution in 1946 in India opened avenues
+        for dairy cooperatives and created an organized dairy sector for production and
+        enterprise development. Livestock keeping in Uttarakhand, like the rest of the
+        country, is a traditional and important source of livelihood. Since the land parcels
+        are a limiting factor in the hills for crop-based agriculture, animal husbandry for
+        dairy products is comparatively a more viable option for hill communities. In
+        Uttarakhand, the nodal agency for the development of dairy is Uttarakhand Co-
+        operative Dairy Federation Limited (UCDFL) and milk unions of the districts.
+      </p>
+
+      <h2>SECTION 1</h2>
+      <h3>WHAT IS ANIMAL HUSBANDRY SECTOR</h3>
+      <h3>WHAT IS DIARY FARMING</h3>
+
+      <h2>SECTION 2</h2>
+      <h3>ASSET REQUIREMENTS</h3>
+      <ol>
+        <li>Land (for sheltering)</li>
+        <li>Cattle (cows, buffaloes, etc.)</li>
+        <li>Cattle feedstock</li>
+        <li>Veterinary health care</li>
+        <li>Connectivity</li>
+        <li>Storage and Processing facilities</li>
+        <li>Water</li>
+      </ol>
+      <h3>SKILL REQUIREMENTS</h3>
+      <ol>
+        <li>Animal keeping (cleaning, grooming, feeding, etc)</li>
+        <li>Healthcare (vaccination, diseases, calving, etc)</li>
+        <li>Dairy products development (Ghee, yogurt, paneer, curd, etc)</li>
+      </ol>
+      <h3>MARKET REQUIREMENTS</h3>
+      <ol>
+        <li>Road and market connectivity</li>
+        <li>Processing units</li>
+        <li>Transport</li>
+        <li>Value addition-equipment and units</li>
+      </ol>
+
+      <h2>SECTION 3</h2>
+      <h3>SCOPE OF THE SUB-SECTOR (CURRENT)</h3>
+      <p>
+        In Uttarakhand, milk cooperatives have 51,121 members comprising villagers and farmers
+        to Aanchal dairy. "Aanchal Dairy"- a flagship milk brand of the state government ensures
+        procurement of milk and payments to the members of cooperatives. In 13 districts of the
+        state, 2551 village-level cooperatives collected two lakh litres of milk every day till May
+        2020 (during lockdown due to coronavirus pandemic). Dairying has been pushed as a
+        high potential with the creation of state dairy development board, Uttarakhand Co-
+        operative Dairy Federation Limited (UCDFL) and milk unions of the districts. Under current
+        initiatives, the dairy department created 11 milk producers SHGs, 10 dairy farms of 2.55
+        lakh litres/day capacity, 45 cold chain units of 1.25 lakh litres/day capacity, animal
+        feedstock in Rudrapur of 100 metric ton capacity, and added 140 more co-operatives
+        making a total of 51,066 dairy members.
+      </p>
+      <p>Ongoing schemes such as:</p>
+      <ol>
+        <li>
+          Ganga Gaya Yojana (2018) in which 4795 women from low socio-economic groups will
+          be given milch cows worth 52,000 INR in which 27000 INR will be state's subsidized,
+        </li>
+        <li>
+          Dugdh Mulya Protsahan Yojana 2018: payments will be given to milk producers and
+          they will be interlinked to dairy co-operations.
+        </li>
+      </ol>
+      <p>
+        At a private level, few of the value addition and milk products-based companies are
+        growing in the hills and are enabling dairy farmers to keep more cattle and produce more
+        milk. The gap between the hills and plains of Uttarakhand is narrowing with the help of
+        entrepreneurship development.
+      </p>
+
+      <h3>SCOPE OF THE SUB-SECTOR (FUTURE)</h3>
+      <p>
+        The growth of the sub-sector is imminent as the consumptive demand for
+        the milk and milk-based products are rising with the rise of population. In
+        Uttarakhand, most of the houses own more than one cattle head and are
+        capable of milching throughout the year. Small scale dairies, enterprises and
+        co-operatives are growing throughout the region along with state-owned
+        Aanchal. Presently, local breeds are dominant in the hills. The livestock as an
+        occupation is diminishing in the hills due to out-migration from the state and
+        rural to urban. Lifestyles change and lack of feedstock supply are major
+        disablers of dairy farming. Few entrepreneurs ventured into commercial feed
+        supply, but the rates of feedstock were not acceptable to the farmers in the
+        hills.
+      </p>
+      <p>
+        Dairy as a sustenance livelihood will flourish only when pre-requisite of the
+        market is met, for example, procurement, feedstock, point of sale, target,
+        local as well as intra-interstate sale, mode of payment needs to be
+        strategically placed into a profit-making. The schemes are minimal and
+        subsidy-based government measures which have average or marginal
+        profits. With the support and marketing-based approach, dairy can be a
+        viable and high potential option.
+      </p>
+
+      <h3>POTENTIAL RISKS/THREATS</h3>
+      <ol>
+        <li>Feedstock unavailability</li>
+        <li>Migration</li>
+        <li>Lifestyle changes</li>
+        <li>Water crisis</li>
+      </ol>
+
+      <h3>LOCAL MARKET OR NOT?</h3>
+      <p>
+        Yes. The local market is there for consumptive use and profit-making in the
+        local market.
+      </p>
+
+      <h2>SECTION 4</h2>
+      <h3>EXISTING OPPORTUNITIES</h3>
+      <ol>
+        <li>UCDF Uttarakhand Co-operative Dairy Federation Ltd. (UCDF Ltd.)</li>
+        <li>National Dairy Research Institute</li>
+      </ol>
+
+      <h3>LOCAL ROLE MODELS - PEOPLE</h3>
+      <ol>
+        <li>Shubham Kabdwal Udhyam</li>
+        <li>Nitin Dayalu IMI / Direct Old Hill Foods</li>
+      </ol>
+
+      <h3>ORGANISATIONS AND INSTITUTIONS</h3>
+      <ol>
+        <li>Himmothhan Society</li>
+        <li>Old Hill Company in Mukteshwar</li>
+      </ol>
+
+      <h3>OPPORTUNITIES FOR LEARNING/TRAINING</h3>
+      <ol>
+        <li>Milk production</li>
+        <li>Processors (Value addition to dairy products)</li>
+        <li>Retail outlet</li>
+        <li>Linking to private enterprises</li>
+        <li>Feedstock supplier</li>
+      </ol>
+
+      <h2>REFERENCES</h2>
+      <ul>
+        <li>
+          Pant et al. (2019). "Problems and prospects of dairy farming in Almora District of Uttarakhand".
+          Journal of Emerging Technologies and Innovative Research.
+        </li>
+        <li>
+          Sharma et al. (2007). "Potential and Prospects of Dairy Business in Uttarakhand: A Case Study of
+          Uttaranchal Cooperative Dairy Federation Limited". Agricultural Economics Research Review.
+        </li>
+        <li>https://dairyvikasuttarakhand.org/, https://dairyvikasuttarakhand.org/faq</li>
+        <li>https://www.ucdfaanchal.org/</li>
+        <li>https://ahd.uk.gov.in/</li>
+        <li>https://dairyvikasuttarakhand.org/fedration</li>
+        <li>https://dairyvikasuttarakhand.org/news/nest-one</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DairyFarmingContent;
